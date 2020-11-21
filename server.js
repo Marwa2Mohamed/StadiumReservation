bb7263db1a1ff7e43596dcd326816c05486961d3
eslam ashraf <e01095097908@gmail.com>
2020-11-20 18:10:42 -0800

    test

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const path = require("path")
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
 const compression = require("compression")
     // define routes
-const userRoutes = require('./Routes/user/user')
 
+const userRoutes = require('./Routes/user/user')
+console.log(process.env.NODE_ENV)
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'Public')));
 
@@ -21,7 +23,7 @@ app.use('/user', userRoutes);
 app.use(compression());
 mongoose.connect('mongodb+srv://eslam:zXL0RmrtxAAMgCls@cluster0.twzmg.mongodb.net/stadium-reservation?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, })
     .then(() => {
-        app.listen(3000, () => {
+        app.listen(PORT, () => {
             console.log(`Server started on port`);
         });
     }).catch(err => {
