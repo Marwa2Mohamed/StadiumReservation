3a9ba901963ac0f1107e9ca70330954389b64967
MarwaMohamed <marwam.2@hotmail.com>
2021-02-08 00:44:14 -0600

    validators are added to signup routes

--- Controllers/auth/auth.js
+++ Controllers/auth/auth.js
@@ -5,81 +5,89 @@ const Player = require('../../Models/users/players_model');
 
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const {check, validationResult} = require('express-validator');
 
 // const accountSid = 'AC01497a7b2e26b1107add63a3796b07c8';
 // const authToken = 'ab2dac19b859780bf3dd2ab7b662fae4';
 // const client = require('twilio')(accountSid, authToken);
 
 exports.signUp = (req, res, next) => {
-    User.find({
-        phone_number: req.body.phone_number
-    }).exec().then(users => {
-        if (users.length > 0) {
-            return res.status(409).json({
-                message: 'phone is exists',
-            });
-        } else {
-            bcrypt.hash(req.body.password, 10, (err, hash) => {
-                if (err) {
-                    return res.status(500).json({
-                        error: err,
-                    });
-                } else {
-                    var user = "";
-                    if (req.body.type_of_user == 1) {
-                        user = new Player({
-                            _id: new mongoose.Types.ObjectId(),
-                            first_name: req.body.first_name,
-                            second_name: req.body.second_name,
-                            phone_number: req.body.phone_number,
-                            password: hash,
-                            actions: true
-
-                        });
-
-                        user.save()
-                        .then(() => {
-                            res.status(201).json({
-                                message: 'user created successfully !',
-
-                            });
-                        })
-                        .catch(err => {
-                            console.log(err);
-                            res.status(500).json({
-                                error: err,
-                            });
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+       return res.status(422).jsonp(errors.array());
+    } // any non-validated error from the routes check methods for phone and password
+     
+    else {
+        User.find({
+            phone_number: req.body.phone_number
+        }).exec().then(users => {
+            if (users.length > 0) {
+                return res.status(409).json({
+                    message: 'phone is exists',
+                });
+            } else {
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if (err) {
+                        return res.status(500).json({
+                            error: err,
                         });
                     } else {
-                        user = new Owner({
-                            _id: new mongoose.Types.ObjectId(),
-                            first_name: req.body.first_name,
-                            second_name: req.body.second_name,
-                            phone_number: req.body.phone_number,
-                            password: hash,
-                            auth: false,
+                        var user = "";
+                        if (req.body.type_of_user == 1) {
+                            user = new Player({
+                                _id: new mongoose.Types.ObjectId(),
+                                first_name: req.body.first_name,
+                                second_name: req.body.second_name,
+                                phone_number: req.body.phone_number,
+                                password: hash,
+                                actions: true
 
-                        });
+                            });
+
+                            user.save()
+                            .then(() => {
+                                res.status(201).json({
+                                    message: 'user created successfully !',
+
+                                });
+                            })
+                            .catch(err => {
+                                console.log(err);
+                                res.status(500).json({
+                                    error: err,
+                                });
+                            });
+                        } else {
+                            user = new Owner({
+                                _id: new mongoose.Types.ObjectId(),
+                                first_name: req.body.first_name,
+                                second_name: req.body.second_name,
+                                phone_number: req.body.phone_number,
+                                password: hash,
+                                auth: false,
+
+                            });
+                            
+                            user.save()
+                            .then(() => {
+                                res.status(201).json({
+                                    message: 'user created successfully !',
+
+                                });
+                            })
+                            .catch(err => {
+                                console.log(err);
+                                res.status(500).json({
+                                    error: err,
+                                });
+                            });
+                        }
                         
-                        user.save()
-                        .then(() => {
-                            res.status(201).json({
-                                message: 'user created successfully !',
-
-                            });
-                        })
-                        .catch(err => {
-                            console.log(err);
-                            res.status(500).json({
-                                error: err,
-                            });
-                        });
                     }
-                    
-                }
+                });
+            }
             });
-        }
-    });
+    }
 };
 
 
