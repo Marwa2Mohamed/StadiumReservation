aee9edcf1e44f73ca6097282f7c293b7df9d8a77
MarwaMohamed <marwam.2@hotmail.com>
2021-03-04 23:29:37 -0600

    weekDaysArray, address, capacity, and available are added as fields

--- Models/playground/playground_model.js
+++ Models/playground/playground_model.js
@@ -1,5 +1,28 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const workingDays = new Schema({
+    dayName: {
+        type: String,
+        required: true
+    },
+
+    start_time: {
+        type: Date,
+        required: true
+    },
+    end_time: {
+        type: Date,
+        required: true
+    },
+    hourPriceAM: {
+        type: Number,
+        required: true
+    },
+    hourPricePM: {
+        type: Number,
+        required: true
+    }
+});
 const playgrounds = new Schema({
     _id: mongoose.Schema.Types.ObjectId,
     playground_name: {
@@ -14,13 +37,9 @@ const playgrounds = new Schema({
         type: String,
        
     },
-    start_time: {
-        type: Date,
-        
-    },
-    end_time: {
-        type: Date,
-
+    weekDays:{
+        type:[workingDays],
+        required:true
     },
     // location: {
     //     type: {
@@ -33,7 +52,24 @@ const playgrounds = new Schema({
     //       required: true
     //     }
     //   },
-    image: { type: String, required: true }
+    address:{
+      type: String,
+      required: true
+    },
+    capacity: {
+        type: String,
+        required: true
+    },
+
+    image: { 
+        type: String, 
+        required: true 
+    },
+
+    avaiable: {
+        type: Boolean,
+        required: true
+    }
 
 });
 
