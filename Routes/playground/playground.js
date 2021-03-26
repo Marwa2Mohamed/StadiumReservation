8e2e638624219768f520ef1d5cf4a4ddcd1a1f3e
MarwaMohamed <marwam.2@hotmail.com>
2021-03-26 00:59:29 -0500

    used req.params in middleware instead of req.body + /deleteWorkingDay, editDescription, editAvailability, editAddress, editWeekDays & addAnotherImage

--- Routes/playground/playground.js
+++ Routes/playground/playground.js
@@ -4,20 +4,22 @@ const upload = require('../../Controllers/uploadingImages');
 const playgroundController = require('../../Controllers/playground/playground_controller');
 const checkAuth = require('../../check_auth/check_auth');
 
-router.post("/addPlayground", checkAuth, upload.array('images[]'), playgroundController.addOwnerPlayground);
+router.post("/addPlayground/:owner_Id", checkAuth, upload.array('images[]'), playgroundController.addOwnerPlayground);
+router.post("/addWorkingDay/:playground_Id", checkAuth, playgroundController.addWorkingDay);
 
-router.get("/getOwnerSpecificPlayground", checkAuth, playgroundController.getOwnerSpecificPlayground);
-router.get("/getOwnerPlaygrounds", checkAuth, playgroundController.getOwnerPlaygrounds);
+router.get("/getOwnerSpecificPlayground/:playground_Id", checkAuth, playgroundController.getOwnerSpecificPlayground);
+router.get("/getOwnerPlaygrounds/:owner_Id", checkAuth, playgroundController.getOwnerPlaygrounds);
 router.get("/getAllPlaygrounds", playgroundController.getAllPlaygrounds);
 
 router.delete("/deleteOwnerPlaygrounds", checkAuth, playgroundController.deleteOwnerPlayground);
-router.delete("/deletePlaygroundImages", checkAuth, playgroundController.deletePlaygroundImages);
+router.delete("/deletePlaygroundImages/:playground_Id", checkAuth, playgroundController.deletePlaygroundImages);
+router.delete("/deleteWorkingDay/:playground_Id",checkAuth, playgroundController.deleteWorkingDay);
 
-router.patch("/editDescription",checkAuth, playgroundController.editDescription);
-router.patch("/editAvailability",checkAuth, playgroundController.editAvailability);
-router.patch("/editAddress",checkAuth, playgroundController.editAddress);
-router.patch("/editWeekDays",checkAuth, playgroundController.editWeekDays);
-router.patch("/addAnotherImage",upload.single('image'),checkAuth, playgroundController.addAnotherImage);
+router.patch("/editDescription/:playground_Id",checkAuth, playgroundController.editDescription);
+router.patch("/editAvailability/:playground_Id",checkAuth, playgroundController.editAvailability);
+router.patch("/editAddress/:playground_Id",checkAuth, playgroundController.editAddress);
+router.patch("/editWeekDays/:playground_Id",checkAuth, playgroundController.editWeekDays);
+router.patch("/addAnotherImage/:playground_Id",upload.single('image'),checkAuth, playgroundController.addAnotherImage);
 
 
 module.exports = router;
